889ff35bfce647c1f241c55cd66f4547e23862ff
jsartist <ja_artist@naver.com>
2020-09-28 00:22:48 +0900

    20200928 - Level 2 ++

---
+++ src/Level_2/FindPrimeNumber/Solution.java
@@ -0,0 +1,10 @@
+package Level_2.FindPrimeNumber;
+
+public class Solution {
+	
+	
+	public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/Level_2/FindPrimeNumber/SolutionMain.java
@@ -0,0 +1,8 @@
+package Level_2.FindPrimeNumber;
+
+public class SolutionMain {
+	public static void main(String[] args) {
+		String numbers = "1037";
+		System.out.println(new Solution().solution(numbers));
+	}
+}
