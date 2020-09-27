518cf977a6ae53c2e2e77eda8adadd89a17832ee
jsartist <js_artist@naver.com>
2020-09-27 23:03:59 +0900

    20200927 - Level 1 ++

---
+++ src/Level_1/PickTwoNumAndAdd/Solution.java
@@ -0,0 +1,37 @@
+package Level_1.PickTwoNumAndAdd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+	private ArrayList<Integer> Add(int[] numbers) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for(int k = 0; k < numbers.length - 1; k++) {
+			for(int j = k + 1; j < numbers.length; j++) {
+				if(!(list.contains(numbers[k] + numbers[j]))) {
+					list.add(numbers[k] + numbers[j]);
+				}
+			}
+		}
+		return list;
+	}
+	
+	private int[] Sorting(ArrayList<Integer> list) {
+		int temp = 0;
+		int c = 0;
+		int[] answer = new int[list.size()];
+		Collections.sort(list);
+		for(int k : list) {
+			answer[c++] = k;
+		}
+		return answer;
+	}
+	
+	public int[] solution(int[] numbers) {
+		ArrayList<Integer> list = Add(numbers);
+		return Sorting(list);
+	}
+	
+	
+}
---
+++ src/Level_1/PickTwoNumAndAdd/SolutionMain.java
@@ -0,0 +1,11 @@
+package Level_1.PickTwoNumAndAdd;
+
+public class SolutionMain {
+	public static void main(String[] args) {
+		int[] num = {2, 1, 3, 4};
+		int[] answer = new Solution().solution(num);
+		for(int k = 0; k < answer.length; k++) {
+			System.out.println(answer[k]);
+		}
+	}
+}
