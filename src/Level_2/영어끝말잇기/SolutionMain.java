964a4c99481be83080dfc149a478fbda86d48218
jsartist <45587406+jsartist@users.noreply.github.com>
2021-08-04 10:41:21 +0900

    Create SolutionMain.java
    20210804 programmers

---
+++ src/Level_2/영어끝말잇기/SolutionMain.java
@@ -0,0 +1,11 @@
+package level2;
+
+public class SolutionMain {
+	public static void main(String[] args) {
+		String[] a = {"hello", "observe", "effect", "take", "either", "decognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+		int[] b = new Solution().solution(5, a);
+		
+		System.out.println(b[0] + " " + b[1]);
+		
+	}
+}
